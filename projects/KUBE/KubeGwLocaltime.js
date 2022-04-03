27d2011c887654ad62fee68b5f54419e42e8118e
subwayfootlong <74981128+subwayfootlong@users.noreply.github.com>
2022-04-03 13:32:24 +0800

    Delete KubeGwLocaltime.js

--- projects/KUBE/KubeGwLocaltime.js
+++
@@ -1,7 +0,0 @@
-//Put this before the deleter
-const input = msg.payload.time;
-let t = 0
-const result = input.replace(/:/g, match => ++t === 3 ? '.' : match)
-const unixtime = new Date(result).valueOf();
-msg.payload.timestamp=unixtime;
-return msg;
