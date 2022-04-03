8e2781ac3b98b0292a4517dbda412218bdd4aa88
subwayfootlong <74981128+subwayfootlong@users.noreply.github.com>
2022-04-03 12:17:42 +0800

    Delete influxdbbatch.bat

--- influxdbbatch.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-C:\Users\Rexroth\Documents\influxdb2-2.1.1-windows-amd64\influxdb2-2.1.1-windows-amd64\influxd --engine-path=E:\influxdbExternal --bolt-path=E:\influxdbExternal\influxd.bolt
