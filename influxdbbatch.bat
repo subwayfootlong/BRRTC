74c23755f06cbf4e0c05e9ebceeb5da92bcf0f2a
subwayfootlong <74981128+subwayfootlong@users.noreply.github.com>
2022-04-03 12:17:27 +0800

    Add files via upload

---
+++ influxdbbatch.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+C:\Users\Rexroth\Documents\influxdb2-2.1.1-windows-amd64\influxdb2-2.1.1-windows-amd64\influxd --engine-path=E:\influxdbExternal --bolt-path=E:\influxdbExternal\influxd.bolt
